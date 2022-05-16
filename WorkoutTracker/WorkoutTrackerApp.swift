f70e7fd5475051815b72b9b83406d8038c9ca8f7
Wilbert Feldman <feldmawa@rose-hulman.edu>
2022-05-16 08:45:06 -0400

    Initial Commit

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Will Feldman on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
